6943472a3807bc69a386d2a2e985b7e68294fda8
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2024-05-21 10:48:55 -0300

    Update AtivosDeUsuarios.java

--- src/AtivosDeUsuarios.java
+++ src/AtivosDeUsuarios.java
@@ -4,7 +4,7 @@ public class AtivosDeUsuarios {
 
     public double __encontrarDinheiroFicticio(){
         // Método consiste em  somente  fingir um valor existente na conta do usuário (visto que não desejo que o usuário insira em "usuario.dinheiro = xxxxx")
-        // Criei este método em outro módulo, pois não faria sentido o usuário ter um dado tão importante armazenado dentro da class Usuário. Portando, seguindo o "D" de SOLID, eu realizei a Inversão de Dependencia
+        // Criei este método em outro módulo, pois não faria sentido o usuário ter um dado tão importante armazenado dentro da class Usuário.
         double valorMinimo = 5000.0;
         double valorMaximo = 15000.0;
 
